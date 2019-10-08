2512aee10efc411a810f7406cc6b10958d028179
Nishant Srivastava <nisrulz@gmail.com>
2019-10-09 00:55:01 +0200

    updated example android tests

--- lantern/src/androidTest/java/github/nisrulz/lantern/ExampleInstrumentedTest.java
+++ lantern/src/androidTest/java/github/nisrulz/lantern/ExampleInstrumentedTest.java
@@ -16,27 +16,29 @@
 
 package github.nisrulz.lantern;
 
-import static org.junit.Assert.*;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
-import org.junit.*;
-import org.junit.runner.*;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Instrumented test, which will execute on an Android device.
+ * <p>
+ * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+class ExampleInstrumentedTest {
 
     @Test
-    public void useAppContext() throws Exception {
+    void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("nisrulz.github.lantern.test", appContext.getPackageName());
+        Context appContext = ApplicationProvider.getApplicationContext();
+        assertEquals("com.github.nisrulz.sensey", appContext.getPackageName());
     }
 }
